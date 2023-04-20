8d6b0d49f5bff2ac5518feabd45ff4f7b67e8900
Jesus Fernandez <Jesus.Fernandez@unican.es>
2023-04-20 18:53:58 +0200

    Add initial version of HTML tables with CV

---
+++ scripts/create-html-datatables.py
@@ -0,0 +1,133 @@
+import json
+import pandas as pd
+
+def addtag(word, field):
+  rval = word
+  if (field == 'comments') and word.startswith('#'):
+    rval = f'<span class="tag">{word[1:]}</span>'
+  elif (field == 'comments') and word.startswith('http'):
+    rval = f'<a href="{word}">{word}</a>'
+  elif (field == 'status') and word in ['selected', 'planned', 'running', 'completed', 'published']:
+    rval = f'<span class="{word}">{word}</span>'
+  return(rval)
+
+def taggify(text, field):
+  rval = text
+  if field in ['status', 'comments']:
+    rval = ' '.join([addtag(x, field) for x in text.split(' ')])
+  return(rval)
+
+def delistify(text):
+  rval = text
+  if type(text) == list:
+    rval = ', '.join(text)
+  return rval
+
+def listify(dic):
+  return {k:[v] for k,v in dic}
+
+def json2datatable(jsonfile, htmlout, top_level, title='', intro='', columns = [], rename_fields = {}, is_1d = False):
+  with open(jsonfile) as f:
+    data = json.load(f)
+  if top_level:
+    data = data[top_level]
+  if is_1d:
+    df = pd.DataFrame(data.values(), index = data.keys()).reset_index()
+    df.columns = [top_level, top_level.replace('_id', '')]
+  else:
+    df = pd.DataFrame(data).transpose()
+  if columns:
+    df = df[columns]
+  field_names = dict(zip(df.columns,df.columns))
+  field_names.update(rename_fields)
+  fp = open(htmlout,'w')
+  fp.write('''<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="author" content="J. Fernandez" />
+<meta name="keywords" content="HTML, CSS, JavaScript" />
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+<script type="text/javascript">
+$(document).ready( function () {
+    $('#table_id').DataTable({
+    });
+} );
+</script>
+''')
+  if title:
+    fp.write(f'<title>{title}</title>')
+  fp.write('''
+<style>
+span.tag {
+  background-color: #c5def5;
+  padding: 0 10px;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 22px !important;
+  border: 1px solid transparent;
+  border-radius: 2em;
+}
+span.selected {color: #3399FF}
+span.planned {color: #FF9999}
+span.running {color: #009900}
+span.completed {color: black; font-weight: bold}
+span.published {color: #3399FF; font-weight: bold}
+span.warning {color: #FF0000; font-weight: bold}
+</style>
+</head>
+<body>
+''')
+  if title:
+    fp.write(f'<h1>{title}</h1>')
+  if intro:
+    fp.write(f'{intro}<p>')
+  fp.write('''
+<table id="table_id" class="display">
+    <thead>
+        <tr>
+''')
+  [fp.write(f'              <th>{field_names[x]}</th>\n') for x in df]
+  fp.write(f'''
+        </tr>
+    </thead>
+    <tbody>
+''')
+  for idx, row in df.iterrows():
+    fp.write(f'        <tr>\n')
+    for field,item in zip(df.columns, row):
+      fp.write(f'            <td>{delistify(item)}</td>\n')
+    fp.write(f'        </tr>\n')
+  fp.write('''
+    </tbody>
+</table>
+</body>
+</html>''')
+  fp.close()
+
+datatable_columns = {
+  'source_id': ['source_id', 'label', 'release_year', 'institution_id', 'activity_participation', 'cohort', 'license'],
+  'institution_id': ['institution_id', 'institution']
+}
+
+is_1d = {
+  'source_id': False,
+  'institution_id': True
+}
+
+text = {
+  'source_id': 'Registered models and license. <span class="warning">This is a test page.</span>',
+  'institution_id': 'Registered institutions. <span class="warning">This is a test page.</span>'
+}
+
+
+if __name__ == '__main__':
+  for cv in ['source_id', 'institution_id']:
+    json2datatable(f'../CORDEX_{cv}.json', f'../docs/CORDEX_{cv}.html', cv,
+      columns = datatable_columns[cv], is_1d = is_1d[cv],
+      title = f'WCRP-CORDEX CORDEX-CMIP6 CV {cv}',
+      intro = text[cv]
+    )
