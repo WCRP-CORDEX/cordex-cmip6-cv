d7a2d7bc86254af3634ef65562c4685ecfc6d688
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-04-11 14:08:42 +0200

    update source_id workflow

--- scripts/update-source_id.py
+++ scripts/update-source_id.py
@@ -1,13 +1,24 @@
 import json
 import sys
+from collections import OrderedDict
 from cordex_cv import update_table
 
 table = "CORDEX-CMIP6_source_id.json"
 table_name = "source_id"
 
 
+licenses = {
+    "CC BY 4.0": "Creative Commons Attribution 4.0 International License (CC BY 4.0; https://creativecommons.org/licenses/by/4.0).",
+    "CC0": "Creative Commons 1.0 Universal (CC0; https://creativecommons.org/publicdomain/zero/1.0).",
+}
+
+
 def update_source_id(entry):
-    return update_table(entry, table, table_name)
+    entry["license"] = licenses[entry["license"]]
+    # entry["institution_id"] = [entry["institution_id"]]
+    entry["cohort"] = ["Registered"]
+    sorted_entry = OrderedDict(sorted(entry.items()))
+    return update_table(sorted_entry, table, table_name)
 
 
 def get_entries(content):
@@ -16,6 +27,13 @@ def get_entries(content):
 
 if __name__ == "__main__":
     content = sys.argv[1]
+    # content = (
+    #    '{"activity_participation": ["DD"], "cohort": '
+    #    '["Registered"], "further_info_url": "https://www.remo-rcm.de", '
+    #    '"institution_id": ["GERICS"], "label": "REMO2020", "label_extended": "REMO '
+    #    'regional model 2020", "license": "CC BY 4.0", '
+    #    '"source_id": "SUPER-RCM", "source_type": "ARCM"}'
+    # )
     # content = (
     #    '{"activity_participation": ["DD"], "cohort": '
     #    '["Registered"], "further_info_url": "https://www.remo-rcm.de", '
