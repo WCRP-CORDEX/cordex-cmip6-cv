92e6805d15a8440a496d21410da9c41162af6c20
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-04-11 12:56:34 +0200

    Update labels.py

--- scripts/labels.py
+++ scripts/labels.py
@@ -7,7 +7,7 @@ result = subprocess.run(command, stdout=subprocess.PIPE)
 labels = result.stdout.decode("utf-8").splitlines()
 
 for lb in labels:
-    table = lb.split("register")[1].strip() if "register" in lb else None
+    table = lb.split("Register")[1].strip() if "Register" in lb else None
     if table:
         break
 print(f"{table}")
