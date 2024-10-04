30b58ce6316b90e2cfe4839d9786fe03526d252c
David Huard <huard.david@ouranos.ca>
2024-10-04 10:34:50 -0400

    Add prefix:

--- scripts/cv2schema.py
+++ scripts/cv2schema.py
@@ -22,16 +22,72 @@ Any missing or incorrect global attribute will raise a `ValidationError`.
 
 DIR = Path(".")
 
+def make_global_attrs_schema(prefix : str=None, enum: bool=False) -> dict:
+    """Create a JSON schema for netCDF global attributes from the JSON CVs.
 
-def read_cv(key: str) -> dict:
-    """Read a CV file and return it as a dictionary."""
-    path = DIR / f"CORDEX-CMIP6_{key}.json"
-    with open(path) as f:
-        return json.load(f)
+    Parameters
+    ----------
+    prefix : str
+        Prefix to add to all properties.
+    enum : bool
+        If True, return an enum schema instead of oneOf, leading to smaller, easier to read schemas.
+
+    Returns
+    -------
+    dict
+        JSON schema for global attributes.
+    """
+    prefix = prefix + ":" if prefix else ""
+
+    # Read required global attributes
+    reqs = read_cv("required_global_attributes")["required_global_attributes"]
+
+    schema = {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "$id": "cmip6-cordex-global-attrs-schema.json#",
+        "title": "CORDEX-CMIP6 metadata schema for global attributes",
+        "description": "JSON schema for global attributes of CORDEX-CMIP6 datasets. This schema is automatically "
+        "generated from the CVs. Manual edits will be overwritten.",
+        "type": "object",
+        "properties": {},
+        "required": [prefix + fid for fid in reqs],
+    }
+
+    integer_fields = []
+
+    formats = {"creation_date": "date-time"}
+
+    props = {}
+    for fid in reqs:
+        if fid in integer_fields:
+            # Could be replaced by patternProperties, but at the expense of readability
+            props[prefix + fid] = {"type": "integer"}
+        else:
+            try:
+                cv = read_cv(fid)
+                for key, val in cv_to_property(cv, enum=enum).items():
+                    props[prefix + key] = val
+            except FileNotFoundError:
+                props[prefix + fid] = {"type": "string"}
+
+        if fid in formats:
+            props[prefix + fid]["format"] = formats[fid]
+
+    schema["properties"].update(props)
+
+    return schema
 
 
-def cv_to_property(cv: dict) -> dict:
-    """Convert a CV to a JSON schema property."""
+def cv_to_property(cv: dict, enum: bool=False) -> dict:
+    """Convert a CV to a JSON schema property.
+
+    Parameters
+    ----------
+    cv : dict
+        CV dictionary.
+    enum: bool
+        If True, return an enum schema instead of oneOf.
+    """
     if len(cv) > 1:
         raise ValueError("CV has more than one key.")
 
@@ -51,53 +107,23 @@ def cv_to_property(cv: dict) -> dict:
                     items.append({"const": key, "title": value})
                 elif isinstance(value, dict):
                     items.append({"const": key, "title": value.get(field[fid], "")})
-            out[fid] = {"oneOf": items}
+            if enum:
+                out[fid] = {"enum": [item["const"] for item in items]}
+            else:
+                out[fid] = {"oneOf": items}
         elif isinstance(keys, list):
             out[fid] = {"enum": keys}
     return out
 
 
-def make_global_attrs_schema() -> dict:
-    """Create a JSON schema for netCDF global attributes from the JSON CVs."""
-
-    # Read required global attributes
-    reqs = read_cv("required_global_attributes")["required_global_attributes"]
-
-    schema = {
-        "$schema": "http://json-schema.org/draft-07/schema#",
-        "title": "CORDEX-CMIP6 metadata schema for global attributes",
-        "description": "JSON schema for global attributes of CORDEX-CMIP6 datasets. This schema is automatically "
-        "generated from the CVs. Manual edits will be overwritten.",
-        "type": "object",
-        "properties": {},
-        "required": reqs,
-    }
-
-    integer_fields = []
-
-    formats = {"creation_date": "date-time"}
-
-    props = {}
-    for fid in reqs:
-        if fid in integer_fields:
-            # Could be replaced by patternProperties, but at the expense of readability
-            props[fid] = {"type": "integer"}
-        else:
-            try:
-                cv = read_cv(fid)
-                props.update(cv_to_property(cv))
-            except FileNotFoundError:
-                props[fid] = {"type": "string"}
-
-        if fid in formats:
-            props[fid]["format"] = formats[fid]
-
-    schema["properties"].update(props)
-
-    return schema
+def read_cv(key: str) -> dict:
+    """Read a CV file and return it as a dictionary."""
+    path = DIR / f"CORDEX-CMIP6_{key}.json"
+    with open(path) as f:
+        return json.load(f)
 
 
 if __name__ == "__main__":
-    schema = make_global_attrs_schema()
+    schema = make_global_attrs_schema(prefix="cordex6", enum=True)
     with open("cmip6-cordex-global-attrs-schema.json", "w") as f:
         json.dump(schema, f, indent=2)
